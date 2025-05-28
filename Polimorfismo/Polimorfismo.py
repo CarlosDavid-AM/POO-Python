aa433dabf1fe528e42c22da77972c5729d88beb0
carlos <apolayamendozacarlos@gmail.com>
2025-05-28 08:50:41 -0500

    polimorfismo

---
+++ Polimorfismo/Polimorfismo.py
@@ -0,0 +1,12 @@
+class Gato():
+    def sonido(self):
+        return "miau"
+    
+class Perro():
+    def sonido(self):
+        return "Guau"
+    
+gato = Gato()
+perro = Perro()
+
+print(gato.sonido())
